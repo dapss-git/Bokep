66f9721d68df2298db6db46732bf58bc36860c86
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:39:26 +0700

    Upload/Edit commands/nsfw/hentai.js via Bot

---
+++ commands/nsfw/hentai.js
@@ -0,0 +1,69 @@
+let handler = async (m, {
+    conn
+}) => {
+    try {
+
+        await conn.sendMessage(m.chat, {
+            react: {
+                text: '⏳',
+                key: m.key
+            }
+        })
+
+        const url = API(
+            'theresav',
+            '/nsfw/hentai', {},
+            'apikey'
+        )
+
+        const res = await fetch(url)
+
+        if (!res.ok) {
+            throw 'Gagal mengambil image'
+        }
+
+        const buffer = Buffer.from(
+            await res.arrayBuffer()
+        )
+
+        await conn.sendMessage(m.chat, {
+            image: buffer,
+            caption: global.botname
+        }, {
+            quoted: m
+        })
+
+        await conn.sendMessage(m.chat, {
+            react: {
+                text: '✅',
+                key: m.key
+            }
+        })
+
+    } catch (e) {
+
+        console.error(e)
+
+        await conn.sendMessage(m.chat, {
+            react: {
+                text: '❌',
+                key: m.key
+            }
+        })
+
+        m.reply(
+            `❌ ${e.message || e}`
+        )
+
+    }
+}
+
+handler.help = ['hentai']
+handler.tags = ['nsfw']
+handler.command = /^(hentai)$/i
+handler.premium = true
+handler.nsfw = true
+handler.register = true
+handler.limit = true
+
+export default handler
