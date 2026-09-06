2100929bfc4dad90f0f5c590d95d664314fd2f52
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:36:25 +0700

    Upload/Edit commands/owner/getdb.js via Bot

---
+++ commands/owner/getdb.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+
+const handler = async (m, {
+    conn,
+    isOwner
+}) => {
+    try {
+        if (!isOwner) return m.reply("*❌ Perintah ini hanya untuk Owner!*");
+
+        const filePath = path.resolve("./database.json");
+
+        if (!fs.existsSync(filePath)) {
+            return m.reply("*❌ File database.json tidak ditemukan*");
+        }
+
+        await conn.sendMessage(
+            m.chat, {
+                document: {
+                    url: filePath
+                },
+                mimetype: "application/json",
+                fileName: "database.json",
+            }, {
+                quoted: m
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        m.reply("*❌ Terjadi error: " + error + "*");
+    }
+};
+
+handler.help = ["getdb"];
+handler.command = ["getdb"];
+handler.owner = true;
+handler.limit = false;
+handler.tags = ["owner"];
+handler.register = true;
+
+export default handler;
