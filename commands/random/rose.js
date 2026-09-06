e65c7113b7862e0f1eca534ee57387bee61faad2
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:48:41 +0700

    Upload/Edit commands/random/rose.js via Bot

---
+++ commands/random/rose.js
@@ -0,0 +1,33 @@
+import { fileTypeFromBuffer } from 'file-type';
+
+let handler = async (m, { conn, usedPrefix, command, Func }) => {
+    await global.loading(m, conn);
+    try {
+        let apiUrl = global.API("theresav", "/random/rose", {}, "apikey");
+
+        let buffer = await Func.fetchBuffer(apiUrl);
+        let { mime } = await fileTypeFromBuffer(buffer);
+        let isVideo = mime.startsWith('video/');
+
+        await conn.sendMessage(m.chat, { 
+            [isVideo ? 'video' : 'image']: buffer, 
+            caption: `✨ *RANDOM ROSE*`,
+            footer: global.footer,
+            buttons: [{ buttonId: `${usedPrefix}${command}`, buttonText: { displayText: 'Next ⏩' }, type: 1 }],
+            viewOnce: true
+        }, { quoted: m });
+
+    } catch (e) {
+        m.reply("❌ Error: " + e.message);
+    } finally {
+        await global.loading(m, conn, true);
+    }
+};
+
+handler.help = ["randomrose"];
+handler.tags = ["random"];
+handler.command = /^randomrose$/i;
+handler.register = true;
+handler.limit = true;
+
+export default handler;
