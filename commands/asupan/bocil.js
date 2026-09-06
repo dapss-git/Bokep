7d576da446ff8b2e08301667e037b6c04434b012
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:36:02 +0700

    Upload/Edit commands/asupan/bocil.js via Bot

---
+++ commands/asupan/bocil.js
@@ -0,0 +1,47 @@
+import { fileTypeFromBuffer } from 'file-type'
+let handler = async (m, { conn, usedPrefix, command, Func }) => {
+    await global.loading(m, conn);
+    try {
+        let apiUrl = global.API("theresav", "/asupan/bocil", {}, "apikey");
+        
+        
+        let buffer = await Func.fetchBuffer(apiUrl);
+        let type = await fileTypeFromBuffer(buffer);
+        let mime = type ? type.mime : 'video/mp4';
+
+        let messageData = { 
+            caption: `✨ *ASUPAN BOCIL*`,
+            footer: global.footer,
+            buttons: [
+                {
+                    buttonId: `${usedPrefix}${command}`,
+                    buttonText: { displayText: 'Next ⏩' },
+                    type: 1
+                }
+            ],
+            viewOnce: true
+        }
+
+        if (mime.startsWith('image/')) {
+            messageData.image = buffer;
+        } else {
+            messageData.video = buffer;
+            messageData.gifPlayback = false;
+        }
+
+        await conn.sendMessage(m.chat, messageData, { quoted: m });
+
+    } catch (e) {
+        m.reply("❌ Error: " + e.message);
+    } finally {
+        await global.loading(m, conn, true);
+    }
+};
+
+handler.help = ["asupanbocil"];
+handler.tags = ["asupan"];
+handler.command = /^asupanbocil$/i;
+handler.register = true;
+handler.limit = true;
+
+export default handler;
