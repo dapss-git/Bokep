acf34e12ceddc533f8b48390f0e9074b0fb63df0
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:36:32 +0700

    Upload/Edit commands/owner/myip.js via Bot

---
+++ commands/owner/myip.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+let handler = async (m, {
+    conn
+}) => {
+    try {
+        const {
+            data
+        } = await axios.get('https://api.ipify.org');
+        conn.reply(m.chat, `IP kamu adalah ${data}`, m);
+    } catch (e) {
+        conn.reply(m.chat, 'Terjadi kesalahan saat memproses permintaan.', m);
+    }
+};
+
+handler.help = ['myip'];
+handler.tags = ['owner'];
+handler.command = /^myip$/i;
+handler.owner = true;
+handler.register = true;
+
+export default handler;
