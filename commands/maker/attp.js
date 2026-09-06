4534ab36880c79f1f0097c6b01e378edaa5e470e
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:47:11 +0700

    Upload/Edit commands/maker/attp.js via Bot

---
+++ commands/maker/attp.js
@@ -0,0 +1,21 @@
+import {
+    sticker
+} from '../../library/sticker.js'
+let handler = async (m, {
+    conn,
+    text
+}) => {
+    if (!text[0]) return m.reply(`Teksnya Mana Kak?\nContoh: .attp undergood`)
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+
+    let stiker = await sticker(null, `https://tenor.googleapis.com/v2/render_dynamic_text?client_key=waffles&key=AIzaSyCbDgY_wZO9guZMktW6MnOGo-nKVFXqaUE&%24alt=proto&text=${encodeURIComponent(text)}&id=258698638&config.output_media_format=2&config.width=320&config.height=200&config.frames_per_second=20&config.composition_type=3`, global.wm, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
+}
+handler.help = ['attp <teks>']
+handler.tags = ['maker']
+handler.command = /^(attp)$/i
+handler.limit = true
+handler.register = true
+
+export default handler
