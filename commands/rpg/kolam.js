feb1e30ed555f124f73ea7c57076bb873eecc858
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:43:47 +0700

    Upload/Edit commands/rpg/kolam.js via Bot

---
+++ commands/rpg/kolam.js
@@ -0,0 +1,36 @@
+let handler = async (m, {
+    conn
+}) => {
+    let user = global.db.data.users[m.sender]
+    let past = `
+╭━━━━「 *BIO* 」   
+┊ *💌 Name :* ${user.registered ? user.name : conn.getName(m.sender)}
+┊ *📊 Level :* ${toRupiah(user.level)}
+┊ *✨ Exp :* ${toRupiah(user.exp)}
+╰═┅═━––––––─ׄ✧
+
+╭━━━━「 *ISI* 」
+┊🦀 Kepiting: ${toRupiah(user.kepiting)}
+┊🦞 Lobster: ${toRupiah(user.lobster)}
+┊🦐 Udang: ${toRupiah(user.udang)}
+┊🦑 Cumi: ${toRupiah(user.cumi)}
+┊🐙 Gurita: ${toRupiah(user.gurita)}
+┊🐡 Buntal: ${toRupiah(user.buntal)}
+┊🐠 Dory: ${toRupiah(user.dory)}
+┊🐳 Orca: ${toRupiah(user.orca)}
+┊🐬 Lumba: ${toRupiah(user.lumba)}
+┊🐋 Paus: ${toRupiah(user.paus)}
+┊🦈 Hiu: ${toRupiah(user.hiu)}
+╰═┅═━––––––─ׄ✧
+🎏 Total Isi: *${toRupiah(user.kepiting + user.lobster + user.udang + user.cumi + user.gurita + user.buntal + user.dory + user.orca + user.lumba + user.paus + user.hiu)}* Jenis`
+    m.reply(past)
+}
+handler.help = ['kolam']
+handler.tags = ['rpg']
+handler.command = /^(kotak(ikan)?|kolam(ikan)?)$/i
+handler.register = true
+handler.group = true
+handler.rpg = true
+export default handler
+
+const toRupiah = number => parseInt(number).toLocaleString().replace(/,/gi, ".")
