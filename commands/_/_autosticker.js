845e50bd3e146c983673cac66d136d23b1f9f8a5
Dapaaaaa <dafapratamaakun4@gmail.com>
2026-09-06 20:49:29 +0700

    Upload/Edit commands/_/_autosticker.js via Bot

---
+++ commands/_/_autosticker.js
@@ -0,0 +1,36 @@
+import { addExif, sticker } from '../../library/sticker.js'
+
+export async function before(m, { conn }) {
+    try {
+        if (m.isBaileys || m.fromMe) return true
+        if (!m.isGroup) return true
+
+        const group = global.db.data.chats[m.chat] || {}
+        if (!group.autosticker) return true
+
+        const q = m.quoted ? m.quoted : m
+        const mime = (q.msg || q).mimetype || q.mediaType || ''
+        if (!mime) return true
+
+        const buffer = await q.download?.()
+        if (!buffer || buffer.length < 1024) return true
+
+        let stiker
+
+        if (/webp/g.test(mime)) {
+            stiker = await addExif(buffer, global.wm || '', global.author || '')
+        } else if (/image/g.test(mime)) {
+            stiker = await sticker(buffer, null, global.wm || '', global.author || '')
+        }
+
+        if (!stiker) return true
+
+        await conn.sendMessage(m.chat, { sticker: stiker })
+
+    } catch (e) {
+        console.error('[AUTOSTICKER ERROR]', e)
+    }
+
+    return true
+}
+
